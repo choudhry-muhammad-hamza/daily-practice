3d9d751fd7955bbef0f3e70fcf0a0108bf36ef27
Muhammad Hamza <choudhrymuhammadhamza@gmail.com>
2026-06-21 23:19:11 +0500

    daily practice

---
+++ js-practice/script.js
@@ -0,0 +1,22 @@
+// let fullName = "muhammad hamza";
+// let age = 24;
+// const price = 27.4568276;
+// let isFollow = true;
+// console.log(isFollow);
+// console.log(price);
+// console.log(fullName);
+// console.log(age);
+
+// let age = 24;
+// console.log(age);
+// console.log(typeof age);
+
+// let fullName = "muhammad hamza";
+// console.log(fullName);
+// let isFollow = false;
+// console.log(isFollow);
+// let x = null;
+// console.log(x);
+
+let ourPlanet = "Earth";
+let currentVisitor = "muhammad hamza";

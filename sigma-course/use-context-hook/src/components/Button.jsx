f6d930a3d16a9e08c2c29eccb5060a0e2ba100c0
Muhammad Hamza <choudhrymuhammadhamza@gmail.com>
2026-07-23 02:15:54 +0500

    daily practice

---
+++ sigma-course/use-context-hook/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Component1 from "./Component1";
+
+const Button = () => {
+  return (
+    <button>
+      <span>
+        <Component1 />
+      </span>
+      i am a button.
+    </button>
+  );
+};
+
+export default Button;

919e85ca9e88c5d6773d174057f1e99dc0e9768e
Muhammad Hamza <choudhrymuhammadhamza@gmail.com>
2026-07-22 15:26:31 +0500

    daily practice

--- sigma-course/react-routing/src/components/About.jsx
+++ sigma-course/react-routing/src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div className="text">About Page.afejwkhfliaehlfihl</div>;
+  return <div className="text">About Page.</div>;
 };
 
 export default About;

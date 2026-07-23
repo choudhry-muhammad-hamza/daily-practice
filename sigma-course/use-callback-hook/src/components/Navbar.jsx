46519292d27b5d6c4d11ecfa2e3509bf187ff13b
Muhammad Hamza <choudhrymuhammadhamza@gmail.com>
2026-07-23 23:19:48 +0500

    daily practice

---
+++ sigma-course/use-callback-hook/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { memo } from "react";
+
+const Navbar = ({ adjective, getAdjective }) => {
+  console.log("Navbar is rendered.");
+
+  return (
+    <>
+      <div>{adjective} Navbar</div>
+      <button
+        onClick={() => {
+          getAdjective();
+        }}
+      >
+        {getAdjective()}
+      </button>
+    </>
+  );
+};
+
+export default memo(Navbar);

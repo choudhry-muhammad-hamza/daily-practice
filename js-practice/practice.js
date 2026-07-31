efc2bb11f3a89f2ab519b4e049aa43910e0806d0
Muhammad Hamza <choudhrymuhammadhamza@gmail.com>
2026-07-31 22:18:43 +0500

    daily practice

--- js-practice/practice.js
+++ js-practice/practice.js
@@ -1,6 +1,17 @@
 document.write("hello hamza! how are you?");
+
 document.write("hello hamza! 2nd time how are you?");
+
 document.getElementsByTagName("h1")[0].innerHTML = "inner text changed";
+
 document.getElementsByTagName("h1")[0].style.background = "red";
+
 document.getElementsByTagName("h1")[0].style.visibility = "hidden";
+
 document.getElementsByTagName("h1")[0].style.display = "none";
+
+console.warn("this is a warning.");
+
+console.error("this is error.");
+
+console.clear();

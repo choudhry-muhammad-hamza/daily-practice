eb5573a3e2829189a334de4e6aabeaef6cbc76fd
Muhammad Hamza <choudhrymuhammadhamza@gmail.com>
2026-08-02 16:25:06 +0500

    daily practice

---
+++ js-practice/clock/script.js
@@ -0,0 +1,24 @@
+let a;
+let date;
+let time;
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  // second: "2-digit",
+};
+setInterval(() => {
+  a = new Date();
+  date = a.toLocaleDateString(undefined, options);
+  time =
+    String(a.getHours()).padStart(2, "0") +
+    ":" +
+    String(a.getMinutes()).padStart(2, "0") +
+    ":" +
+    String(a.getSeconds()).padStart(2, "0");
+  // time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+  // ":" +
+  // a.getMilliseconds();
+  document.getElementById("time").innerHTML = time + " on date " + date;
+}, 1000);

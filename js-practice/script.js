3e2f6407c32951974035f9f773901c685f01360e
Muhammad Hamza <choudhrymuhammadhamza@gmail.com>
2026-08-07 11:39:03 +0500

    daily practice

--- js-practice/script.js
+++ js-practice/script.js
@@ -1005,7 +1005,7 @@
 //   console.log("getting data 2 ...");
 //   getData(2, () => {
 //     console.log("getting data 3 ...");
-//     getData(3, () => {d
+//     getData(3, () => {
 //       console.log("getting data 4 ...");
 //       getData(4);
 //     });

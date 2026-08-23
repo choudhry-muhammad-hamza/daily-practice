9d12109d572258ca84c5b0b303ea89d46e88d13c
Muhammad Hamza <choudhrymuhammadhamza@gmail.com>
2026-08-23 22:25:21 +0500

    daily practice

--- sigma-course/react-routing/src/App.jsx
+++ sigma-course/react-routing/src/App.jsx
@@ -36,15 +36,15 @@ function App() {
         </>
       ),
     },
-    {
-      path: "/contact",
-      element: (
-        <>
-          <Navbar />
-          <Contact />
-        </>
-      ),
-    },
+    // {
+    //   path: "/contact",
+    //   element: (
+    //     <>
+    //       <Navbar />
+    //       <Contact />
+    //     </>
+    //   ),
+    // },
     {
       path: "/user/:username",
       element: (
